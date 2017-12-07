08e8973cdaeeea75b93bf245bc43c2f7ab044417
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-07 18:34:59 -0500

    Update UnexpectedValueException.php

--- src/Exception/UnexpectedValueException.php
+++ src/Exception/UnexpectedValueException.php
@@ -1,1 +1,15 @@
+<?php
+/*
+ * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository
+ * @copyright Copyright (c) 2017-2017 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License
+ */
+ 
+namespace Genial\Cryptography\Exception;
 
+/**
+ * UnexpectedValueException.
+ */
+class UnexpectedValueException extends \UnexpectedValueException implements ExceptionInterface
+{
+}

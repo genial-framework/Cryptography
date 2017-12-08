886a89bfa30aa741bf0aecc646b9cb4929a6cae2
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-07 23:15:42 -0500

    Update PasswordHash.php

--- src/Password/PasswordHash.php
+++ src/Password/PasswordHash.php
@@ -1,1 +1,32 @@
+<?php
+/*
+ * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository
+ * @copyright Copyright (c) 2017-2017 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License
+ */
+ 
+namespace Genial\Cryptography\Password;
 
+use Genial\Cryptography\Option\Password\AlgoList;
+use Genial\Cryptography\Hash;
+
+/**
+ * PasswordHash.
+ */
+class PasswordHash
+{
+    const COST = 11;
+    
+    const ALGO = \PASSWORD_BCRYPT;
+    
+    public static function cipher(string $plainTestPassword, $const_algorithm = self::ALGO, $cost = self::COST)
+    {
+        
+    }
+    
+    public static function verify(string $userInputPassword, string $hash)
+    {
+        return password_verify($userInputPassword, $hash);
+    }
+    
+}

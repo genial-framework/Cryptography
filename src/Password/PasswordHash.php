0bae902c47168b66a10fd7e2816933f9c1cc4f6e
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-08 00:37:23 -0500

    Update PasswordHash.php

--- src/Password/PasswordHash.php
+++ src/Password/PasswordHash.php
@@ -7,6 +7,7 @@
  
 namespace Genial\Cryptography\Password;
 
+use Genial\Cryptography\Exception\InvalidArgumentException;
 use Genial\Cryptography\Option\Password\AlgoList;
 use Genial\Cryptography\Hash;
 
@@ -15,13 +16,100 @@ use Genial\Cryptography\Hash;
  */
 class PasswordHash
 {
+    const ALGO = PASSWORD_BCRYPT;
+ 
     const COST = 11;
+ 
+    const TIME_COST = PASSWORD_ARGON2_DEFAULT_TIME_COST;
+ 
+    const MEMORY_COST = PASSWORD_ARGON2_DEFAULT_MEMORY_COST;
+ 
+    const THREADS = PASSWORD_ARGON2_DEFAULT_THREADS;
     
-    const ALGO = \PASSWORD_BCRYPT;
-    
-    public static function cipher(string $plainTestPassword, $const_algorithm = self::ALGO, $cost = self::COST)
+    public static function cipher(string $plainTextPassword, $const_algorithm = self::ALGO, $cost = self::COST, $memory_cost = self::MEMORY_COST, $threads = self::THREADS)
     {
-        
+        if (!is_int($cost))
+        {
+            throw new InvalidArgumentException(sprintf(
+                '"%s" - "$cost" is not an integer.',
+                __METHOD__
+            ));
+        }
+        if (!is_int($memory_cost))
+        {
+            throw new InvalidArgumentException(sprintf(
+                '"%s" - "$memory_cost" is not an integer.',
+                __METHOD__
+            ));
+        }
+        if (!is_int($threads))
+        {
+            throw new InvalidArgumentException(sprintf(
+                '"%s" - "$threads" is not an integer.',
+                __METHOD__
+            ));
+        }
+        if (defined('PASSWORD_ARGON2I') && $const_algorithm == PASSWORD_ARGON2I)
+        {
+            $options = [
+            ];
+            if ($memory_cost != MEMORY_COST)
+            {
+                $options += [
+                    'memory_cost' => $memory_cost;
+                ];
+            }
+            if ($cost != TIME_COST)
+            {
+                $options += [
+                    'time_cost' => $cost;
+                ];
+            }
+            if ($threads != THREADS)
+            {
+                $options += [
+                    'threads' => $cost;
+                ];
+            }
+            if (!empty($options))
+            {
+                return password_hash($plainTextPassword, $const_algorithm, $options);
+            }
+            else
+            {
+                return password_hash($plainTextPassword, $const_algorithm);
+            }
+        }
+        elseif ($const_algorithm = PASSWORD_BCRYPT)
+        {
+            $options = [
+            ];
+            if ($cost != COST)
+            {
+                $options += [
+                    'cost' => $cost;
+                ];
+            }
+            if (!empty($options))
+            {
+                return password_hash($plainTextPassword, $const_algorithm, $options);
+            }
+            else
+            {
+                return password_hash($plainTextPassword, $const_algorithm);
+            }
+        }
+        elseif ($const_algorithm = PASSWORD_DEFAULT)
+        {
+            return password_hash($plainTextPassword, $const_algorithm);
+        }
+        else
+        {
+            throw new UnexpectedValueException(sprintf(
+                '"%s" - "$const_algorithm" is not a valid password hash algorithm.',
+                __METHOD__
+            ));
+        }
     }
     
     public static function verify(string $userInputPassword, string $hash)

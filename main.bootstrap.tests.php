bede07e69e915673f631f55628c31c7ab004e27c
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-08 05:00:28 -0500

    Update main.bootstrap.tests.php

--- main.bootstrap.tests.php
+++ main.bootstrap.tests.php
@@ -4,7 +4,7 @@
 //    PREVENTS ERRORS DURING RUNTIME     //
 //---------------------------------------//
 
-defined('PASSWORD_ARGON2I', 2) || define('PASSWORD_ARGON2I', 2);
+defined('PASSWORD_ARGON2I') || define('PASSWORD_ARGON2I', 2);
 
 defined('PASSWORD_ARGON2_DEFAULT_TIME_COST') || define('PASSWORD_ARGON2_DEFAULT_TIME_COST', 2);
 defined('PASSWORD_ARGON2_DEFAULT_MEMORY_COST') || define('PASSWORD_ARGON2_DEFAULT_MEMORY_COST', 2);

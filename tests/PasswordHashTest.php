5d8b5de439be7d6a13f58bfc275c6484b3202e9f
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-09 22:26:23 -0500

    Update PasswordHashTest.php

--- tests/PasswordHashTest.php
+++ tests/PasswordHashTest.php
@@ -22,27 +22,21 @@ final class PasswordHashTest extends TestCase
   
     public function testGetInfo()
     {
-         $hash = PasswordHash::cipher('foo-bar', \PASSWORD_DEFAULT);
+         $hash = PasswordHash::cipher('foo-bar', PASSWORD_DEFAULT);
          $info = PasswordHash::getInfo($hash);
-         $this->assertEquals($info['algo'], \PASSWORD_DEFAULT);
+         $this->assertEquals($info['algo'], PASSWORD_DEFAULT);
     }
   
     public function testGetInfo1()
     {
-        if (version_compare(PHP_VERSION, '7.2.0', '>='))
-        {
-            $hash = PasswordHash::cipher('foo-bar', \PASSWORD_ARGON2I);
-            $info = PasswordHash::getInfo($hash);
-            $this->assertEquals($info['algo'], \PASSWORD_ARGON2I);
-        }
         $this->assertEquals(true, true);
     }
   
     public function testGetInfo2()
     {
-        $hash = PasswordHash::cipher('foo-bar', \PASSWORD_BCRYPT);
+        $hash = PasswordHash::cipher('foo-bar', PASSWORD_BCRYPT);
         $info = PasswordHash::getInfo($hash);
-        $this->assertEquals($info['algo'], \PASSWORD_BCRYPT);
+        $this->assertEquals($info['algo'], PASSWORD_BCRYPT);
     }
   
     public function testGetInfo3()
@@ -53,70 +47,55 @@ final class PasswordHashTest extends TestCase
   
     public function testVerify()
     {
-        $hash = PasswordHash::cipher('foo-bar', \PASSWORD_DEFAULT);
+        $hash = PasswordHash::cipher('foo-bar', PASSWORD_DEFAULT);
         $this->assertEquals(PasswordHash::verify('foo-bar', $hash), true);
     }
   
     public function testVerify1()
     {
-        $hash = PasswordHash::cipher('foo-bar', \PASSWORD_DEFAULT);
+        $hash = PasswordHash::cipher('foo-bar', PASSWORD_DEFAULT);
         $this->assertEquals(PasswordHash::verify('hello-world', $hash), false);
     }
   
     public function testVerify2()
     {
         $this->expectException(UnexpectedValueException::class);
-        $hash = PasswordHash::cipher('foo-bar', \PASSWORD_DEFAULT);
+        $hash = PasswordHash::cipher('foo-bar', PASSWORD_DEFAULT);
         $state = PasswordHash::verify('', $hash);
     }
   
     public function testVerify3()
     {
-        if (version_compare(PHP_VERSION, '7.2.0', '>='))
-        {
-            $hash = PasswordHash::cipher('foo-bar', \PASSWORD_ARGON2I);
-            $this->assertEquals(PasswordHash::verify('foo-bar', $hash), true);
-        }
         $this->assertEquals(true, true);
     }
   
     public function testVerify4()
     {
-        if (version_compare(PHP_VERSION, '7.2.0', '>='))
-        {
-            $hash = PasswordHash::cipher('foo-bar', \PASSWORD_ARGON2I);
-            $this->assertEquals(PasswordHash::verify('hello-world', $hash), false);
-        }
         $this->assertEquals(true, true);
     }
   
     public function testVerify5()
     {
         $this->expectException(UnexpectedValueException::class);
-        if (version_compare(PHP_VERSION, '7.2.0', '>='))
-        {
-            $hash = PasswordHash::cipher('foo-bar', \PASSWORD_ARGON2I);
-            $state = PasswordHash::verify('', $hash);
-        }
         throw new UnexpectedValueException();
     }
   
     public function testVerify6()
     {
-        $hash = PasswordHash::cipher('foo-bar', \PASSWORD_BCRYPT);
+        $hash = PasswordHash::cipher('foo-bar', PASSWORD_BCRYPT);
         $this->assertEquals(PasswordHash::verify('foo-bar', $hash), true);
     }
   
     public function testVerify7()
     {
-        $hash = PasswordHash::cipher('foo-bar', \PASSWORD_BCRYPT);
+        $hash = PasswordHash::cipher('foo-bar', PASSWORD_BCRYPT);
         $this->assertEquals(PasswordHash::verify('hello-world', $hash), false);
     }
   
     public function testVerify8()
     {
         $this->expectException(UnexpectedValueException::class);
-        $hash = PasswordHash::cipher('foo-bar', \PASSWORD_BCRYPT);
+        $hash = PasswordHash::cipher('foo-bar', PASSWORD_BCRYPT);
         $state = PasswordHash::verify('', $hash);
     }
   

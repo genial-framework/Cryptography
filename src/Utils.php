7cdd896476f31af8d2405579469895a62698610c
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-04 21:35:43 -0500

    Add some util functions

--- src/Utils.php
+++ src/Utils.php
@@ -1,8 +1,13 @@
 <?php
-/*
- * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository
- * @copyright Copyright (c) 2017-2017 Genial Framework. <https://github.com/Genial-Framework>
- * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License.
  */
 
 namespace Genial\Cryptography;
@@ -12,13 +17,44 @@ namespace Genial\Cryptography;
  */
 class Utils
 {
-    public static function HashHmacAlgos()
+    
+    /**
+     * hashEquals().
+     *
+     * Timing attack safe string comparison.
+     *
+     * @param string $knownString The string of known length to compare against.
+     * @param string $userString  The user-supplied string.
+     *
+     * @return bool Returns TRUE when the two strings are equal, FALSE otherwise.
+     */
+    public static function hashEquals(string $knownString, string $userString)
     {
-        if (function_exists('hash_hmac_algos'))
-        {
-            return hash_hmac_algos();
-        }
+        return hash_equals($knownString, $userString);
+    }
+    
+    /**
+     * hashAlgos().
+     *
+     * Return a list of registered hashing algorithms.
+     *
+     * @return array Returns a numerically indexed array containing the list of supported hashing algorithms.
+     */
+    public static function hashAlgos()
+    {
         return hash_algos();
+    }
+    
+    /**
+     * hashHmacAlgos().
+     *
+     * Return a list of registered hashing algorithms suitable for hash_hmac.
+     *
+     * @return array Returns a numerically indexed array containing the list of supported hashing algorithms suitable for hash_hmac().
+     */
+    public static function hashHmacAlgos()
+    {
+        return hash_hmac_algos();
     }
     
 }

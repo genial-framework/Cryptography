d030b695a0ccdd2275cbc1454922c1f0368f2f0e
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-04 22:12:57 -0500

    Update UtilsTest.php

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -1,8 +1,8 @@
 <?php
-/*
- * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository
- * @copyright Copyright (c) 2017-2017 Genial Framework. <https://github.com/Genial-Framework>
- * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License
+/**
+ * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Components>
+ * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License.
  */
 
 namespace Genial\Cryptography;
@@ -14,22 +14,31 @@ use PHPUnit\Framework\TestCase;
  */
 final class UtilsTest extends TestCase
 {
-    public function testHashHmacAlgos()
+    
+    public function testHashEquals()
     {
-        if (!version_compare(PHP_VERSION, '7.2.0', '>='))
-        {
-            $this->assertEquals(Utils::HashHmacAlgos(), hash_algos());
-        }
-        $this->assertEquals(true, true);
+        $hashA = "\xF0\x9D\x92\xB3" . "\xF0\x9D\xA5\xB3" .
+            "\xF0\x9D\x92\xB3" . "\xF0\x9D\xA5\xB3" .
+            "\xF0\x9D\x92\xB3" . "\xF0\x9D\xA5\xB3" .
+            "\xF0\x9D\x92\xB3" . "\xF0\x9D\xA5\xB3";
+        $hashB = "\xF0\x9D\x92\xB3" . "\xF0\x9D\xA5\xB3" .
+            "\xF0\xAD\x9F\xC0" . "\xF0\xAD\x9F\xC0" .
+            "\xF0\xAD\x9F\xC0" . "\xF0\xAD\x9F\xC0" .
+            "\xF0\xAD\x9F\xC0" . "\xF0\xAD\x9F\xC0";
+        $hashC = "d9d38fdfd97aeffc71abd309b6548ca3d602b8e721c0ee14edd432038c41c292";
+        $hashD = "d9d38fdfd97aeffc71abd309b6548ca3d602b8e721c0ee14edd432038c41c292";
+        $this->assertTrue(!Utils::hashEquals($hashA, $hashB));
+        $this->assertTrue(Utils::hashEquals($hashC, $hashD));
     }
     
-    public function testHashHmacAlgos1()
+    public function testHashAlgos()
     {
-        if (version_compare(PHP_VERSION, '7.2.0', '>='))
-        {
-            $this->assertEquals(Utils::HashHmacAlgos(), hash_hmac_algos());
-        }
-        $this->assertEquals(true, true);
+        $this->assertEquals(hash_algos(), Utils::hashAlgos());
+    }
+    
+    public function testHashHmacAlgos()
+    {
+        $this->assertEquals(hash_hmac_algos(), Utils::hashAlgos());
     }
   
 }

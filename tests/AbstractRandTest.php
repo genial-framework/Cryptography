27defd80868541b2080ab28b30e50b8a758b59d8
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-04 22:31:31 -0500

    Update AbstractRandTest.php

--- tests/AbstractRandTest.php
+++ tests/AbstractRandTest.php
@@ -1,1 +1,34 @@
+<?php
+/**
+ * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Components>
+ * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Cryptography;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * AbstractRandTest.
+ */
+final class AbstractRandTest extends TestCase
+{
+  
+    public function testRandomInt()
+    {
+        $int = Utils::randomInt();
+        $this->assertTrue(is_int($int));
+        $this->assertTrue(strlen(10, 99) === 2);
+        $this->assertTrue(!(strlen(10, 99) === 3));
+    }
+    
+    public function testRandomBytes()
+    {
+        $bytes = Utils::randomBytes(5);
+        $this->assertTrue(is_string($bytes));
+        $this->assertTrue(strlen($bytes) === 5);
+        $this->assertTrue(!(strlen($bytes) === 6));
+    }
+  
+}

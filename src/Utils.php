a082e7d83ae86e238c1405a1b0e7b2d5d032cff5
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-04 21:43:46 -0500

    Update Utils.php

--- src/Utils.php
+++ src/Utils.php
@@ -1,9 +1,7 @@
 <?php
 /**
- * Genial Framework.
- *
  * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository.
- * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Framework>
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Components>
  * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License.
  */
 

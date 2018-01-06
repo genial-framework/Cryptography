c26121e24068946eb486e1180890755d8a5c3c9b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-05 20:59:36 -0500

    Update Lovell.php

--- src/Password/Lovell.php
+++ src/Password/Lovell.php
@@ -1,1 +1,18 @@
+<?php
+/**
+ * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Components>
+ * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License.
+ */
 
+namespace Genial\Cryptography\Password;
+
+use Genial\Cryptogrphy\{
+    HashHmac,
+    Hash
+};
+
+class Lovell implements LovellInterface
+{
+  
+}

dd5bd88edc2d82c4a3c182d2e96e02b32e29f6d5
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-05 19:41:25 -0500

    Update HashHmac.php

--- src/HashHmac.php
+++ src/HashHmac.php
@@ -7,72 +7,112 @@
  
 namespace Genial\Cryptography;
 
-use Genial\Cryptography\Exception\UnexpectedValueException;
-use Genial\Cryptography\Option\AlgoList;
-
 /**
  * HashHmac.
  */
 class HashHmac
 {
+ 
+    /**
+     * @const bool|false RAW_OUTPUT.
+     */
     const RAW_OUTPUT = false;
     
-    protected static $lastAlgorithmSupported;
+    /**
+     * @var string|null $cachedAlgo The last supported algorithm used.
+     */
+    protected static $cachedAlgo = null;
     
-    public static function cipher(string $hashAlgorithm, string $data, string $key, $rawOutput = self::RAW_OUTPUT)
+    /**
+     * cipher().
+     *
+     * Generate a keyed hash value using the HMAC method.
+     *
+     * @param string $hashAlgo      Name of selected hashing algorithm (i.e. "md5", "sha256", "haval160,4", etc..)
+     * @param string $data          Message to be hashed.
+     * @param string $sharedKey     Shared secret key used for generating the HMAC variant of the message digest.
+     * @param bool|false $rawOutput When set to TRUE, outputs raw binary data. FALSE outputs lowercase hexits.
+     *
+     * @throws UnexpectedValueException If the algorithm is not supported.
+     *
+     * @return string Returns a string containing the calculated message digest as lowercase hexits unless raw_output is set to true in
+     *                which case the raw binary representation of the message digest is returned.
+     */
+    public static function cipher(string $hashAlgo, string $data, string $sharedKey, $rawOutput = self::RAW_OUTPUT)
     {
-        if (self::isSupportedAlgo($hashAlgorithm))
+        if (self::supportedAlgo($hashAlgo))
         {
-            self::$lastAlgorithmSupported = $hashAlgorithm;
-            return hash_hmac($hashAlgorithm, $data, $key, $rawOutput);
-        }
-        else
+            self::$cachedAlgo = $hashAlgo;
+            return hash_hmac($hashAlgo, $data, $sharedKey, boolval($rawOutput));
+        } else
         {
-            throw new UnexpectedValueException(sprintf(
-                '"%s" - "$hashAlgorithm" is not supported.',
-                __METHOD__
+            throw new Exception\UnexpectedValueException(sprintf(
+                '`%s` `%s` is not supported.',
+                __METHOD__,
+                htmlspecialchars($hashAlgo, ENT_QUOTES)
             ));
         }
     }
     
-    public static function getOutputSize(string $algorithm, string $hash, string $key, $rawOutput = self::RAW_OUTPUT)
+    /**
+     * getOutputSize().
+     *
+     * Get the hexits or raw output binary size of the given hash.
+     *
+     * @param string $data The hash to test.
+     *
+     * @return bool Returns the output size of the hash.
+     */
+    public static function getOutputSize(string $hash)
     {
-        if (self::isSupportedAlgo($algorithm))
-        {
-            self::$lastAlgorithmSupported = $algorithm;
-            return mb_strlen(self::cipher($algorithm, $hash, $key, $rawOutput), '8bit');
-        }
-        else
-        {
-            throw new UnexpectedValueException(sprintf(
-                '"%s" - "$algorithm" is not supported.',
-                __METHOD__
-            ));
-        }
+        return Hash::getOutputSize($hash);
     }
     
-    public static function isSupportedAlgo(string $algorithm)
+    /**
+     * supportedAlgo().
+     *
+     * Check to see if the given algorithm is supported.
+     *
+     * @param string $algo Name of selected hashing algorithm (e.g. "md5", "sha256", "haval160,4", etc..).
+     *
+     * @return bool Returns TRUE if the algorithm is supported otherwise return FALSE.
+     */
+    public static function supportedAlgo(string $algo)
     {
-        if ($algorithm === self::$lastAlgorithmSupported)
+        if ($algo === self::$cachedAlgo)
         {
             return true;
         }
-        if (in_array($algorithm, AlgoList::HashHmacAlgos(), true))
+        if (in_array($algo, Utils::hashHmacAlgos(), true))
         {
-            self::$lastAlgorithmSupported = $algorithm;
+            self::$cachedAlgo = $algo;
             return true;
         }
         return false;
     }
     
-    public static function clearLastAlgorithmCache()
+    /**
+     * clearCachedAlgo().
+     *
+     * Clear the supported algorithm cache.
+     *
+     * @return void.
+     */
+    public static function clearCachedAlgo()
     {
-        self::$lastAlgorithmSupported = null;
+        self::$cachedAlgo = null;
     }
  
-    public static function getLastSupportedAlgorithm()
+    /**
+     * getCacheAlgo().
+     *
+     * Get the supported algorithm based on cache.
+     *
+     * @return string Returns the last algorithm used that is supported.
+     */
+    public static function getCacheAlgo()
     {
-        return self::$lastAlgorithmSupported;
+        return self::$cachedAlgo;
     }
     
 }

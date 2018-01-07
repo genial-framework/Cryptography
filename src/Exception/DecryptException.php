01beb3aeef335fa59ee9c5463a3eb4aa64eb91dc
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-07 17:27:20 -0500

    Update DecryptException.php

--- src/Exception/DecryptException.php
+++ src/Exception/DecryptException.php
@@ -1,1 +1,16 @@
+<?php
+/**
+ * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Components>
+ * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Genial\Cryptography\Exception;
 
+/**
+ * DecryptException.
+ */
+class DecryptException extends \RuntimeException implements CryptographyExceptionInterface, ExceptionInterface
+{
+ 
+}

3c138349dbd405867d3456bb0d98a6d18627ca2a
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-07 17:27:44 -0500

    Update EncryptException.php

--- src/Exception/EncryptException.php
+++ src/Exception/EncryptException.php
@@ -1,1 +1,16 @@
+<?php
+/**
+ * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Components>
+ * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Genial\Cryptography\Exception;
 
+/**
+ * EncryptException.
+ */
+class EncryptException extends \RuntimeException implements CryptographyExceptionInterface, ExceptionInterface
+{
+ 
+}

aab21145ce096d5feb59c4a6d28642c3b5a3612b
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-08 17:59:05 -0500

    Change comment block header

--- src/Password/Bcrypt.php
+++ src/Password/Bcrypt.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link      <https://github.com/Genial-Components/Cryptography> for the canonical source repository.
- * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Framework>
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
  * @license   <https://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
  */
  

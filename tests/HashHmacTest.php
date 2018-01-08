25a1234732fc25d18adae9966442f1fa0537a192
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-08 18:24:36 -0500

    Update HashHmacTest.php

--- tests/HashHmacTest.php
+++ tests/HashHmacTest.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository.
- * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Components>
- * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License.
+ * @link      <https://github.com/Genial-Components/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
  */
  
 namespace Genial\Cryptography;

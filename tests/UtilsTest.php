3aeefb070904bde5d2a2ba2eb7906e7cacd71c9a
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-08 20:07:40 -0500

    Update UtilsTest.php

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * @link      <https://github.com/Genial-Framework/Cryptography> for the canonical source repository.
- * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Components>
- * @license   <https://github.com/Genial-Framework/Cryptography/blob/master/LICENSE> New BSD License.
+ * @link      <https://github.com/Genial-Components/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
  */
 
 namespace Genial\Cryptography;
 
-use PHPUnit\Framework\TestCase;
+use \PHPUnit\Framework\TestCase;
 
 /**
  * UtilsTest.
@@ -38,13 +38,7 @@ final class UtilsTest extends TestCase
     
     public function testHashHmacAlgos()
     {
-        if (\function_exists('hash_hmac_algos'))
-        {
-            $this->assertEquals(\hash_hmac_algos(), Utils::hashHmacAlgos());
-        } else
-        {
-            $this->assertEquals(\hash_algos(), Utils::hashHmacAlgos());
-        }
+        $this->assertEquals(\hash_hmac_algos(), Utils::hashHmacAlgos());
     }
   
 }

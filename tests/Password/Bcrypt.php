398f28559dbb0cc6df0a28ca90226f32c6e41c76
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-08 20:08:28 -0500

    Update Bcrypt.php

--- tests/Password/Bcrypt.php
+++ tests/Password/Bcrypt.php
@@ -1,2 +1,8 @@
 <?php
+/**
+ * @link      <https://github.com/Genial-Components/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
+ */
+
 

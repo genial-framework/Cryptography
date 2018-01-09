2ea4250ccf83bb203db30b788b5ed09509a34062
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-08 20:09:41 -0500

    Update Bcrypt.php

--- tests/Password/Bcrypt.php
+++ tests/Password/Bcrypt.php
@@ -5,4 +5,19 @@
  * @license   <https://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
  */
 
+namespace Genial\Cryptography;
 
+use \PHPUnit\Framework\TestCase;
+
+use \Genial\Cryptography\
+{
+    UnexpectedValueException
+};
+
+/**
+ * HashTest.
+ */
+final class BcryptTest extends TestCase
+{
+  
+}

01b433bcdb352b2d34d5963c8818763e6b4631a6
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-09 15:58:13 -0500

    Update Argon2i.php

--- src/Password/Argon2i.php
+++ src/Password/Argon2i.php
@@ -1,1 +1,38 @@
+<?php
+/**
+ * @link      <https://github.com/Genial-Components/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Genial\Cryptography\Password;
 
+use \Genial\Cryptography\
+{
+    Utils,
+    Exception\RangeException,
+    Exception\LengthException
+};
+
+/**
+ * Argon2i.
+ */
+class Argon2i extends AbstractPasswordHash implements PasswordHashInterface
+{
+    
+    /**
+     * @var int $cost The memory cost `Argon2i` should use during execution.
+     */
+    private $memoryCost = \PASSWORD_ARGON2_DEFAULT_MEMORY_COST;
+    
+    /**
+     * @var int $cost The cost `Argon2i` should use during execution.
+     */
+    private $timeCost = \PASSWORD_ARGON2_DEFAULT_TIME_COST;
+    
+    /**
+     * @var int $cost The threads `Argon2i` should use during execution.
+     */
+    private $threads = \PASSWORD_ARGON2_DEFAULT_THREADS;
+  
+}

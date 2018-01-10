cd4facd714398c2dee06c5317beefaaf761f21b5
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-09 21:17:04 -0500

    Clean file.

--- src/Password/Argon2i.php
+++ src/Password/Argon2i.php
@@ -1,18 +1,13 @@
 <?php
 /**
  * @link      <https://github.com/Genial-Components/Cryptography> for the canonical source repository.
- * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Framework>
  * @license   <https://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
  */
  
-namespace Genial\Cryptography\Password;
+namespace Genial\Crypt\Password;
 
-use \Genial\Cryptography\
-{
-    Utils,
-    Exception\RangeException,
-    Exception\LengthException
-};
+use \Genial\Crypt\Exception;
 
 /**
  * Argon2i.
@@ -21,17 +16,17 @@ class Argon2i extends AbstractPasswordHash implements PasswordHashInterface
 {
     
     /**
-     * @var int $cost The memory cost `Argon2i` should use during execution.
+     * @var int $cost|PASSWORD_ARGON2_DEFAULT_MEMORY_COST The memory cost `Argon2i` should use during execution.
      */
     private $memoryCost = \PASSWORD_ARGON2_DEFAULT_MEMORY_COST;
     
     /**
-     * @var int $cost The time cost `Argon2i` should use during execution.
+     * @var int $cost|PASSWORD_ARGON2_DEFAULT_TIME_COST The time cost `Argon2i` should use during execution.
      */
     private $timeCost = \PASSWORD_ARGON2_DEFAULT_TIME_COST;
     
     /**
-     * @var int $cost The threads `Argon2i` should use during execution.
+     * @var int $cost|PASSWORD_ARGON2_DEFAULT_THREADS The threads `Argon2i` should use during execution.
      */
     private $threads = \PASSWORD_ARGON2_DEFAULT_THREADS;
   

246056f3d93c8697873c2c148cf3d14886db7a1c
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-09 21:03:32 -0500

    Update ExceptionInterface.php

--- src/Exception/ExceptionInterface.php
+++ src/Exception/ExceptionInterface.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * @link      <https://github.com/Genial-Components/Cryptography> for the canonical source repository.
- * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Framework>
  * @license   <https://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
  */
  
-namespace Genial\Cryptography\Exception;
+namespace Genial\Crypt\Exception;
 
 /**
  * ExceptionInterface.

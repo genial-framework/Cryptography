1711127f07b5cb048ce83eda4c3c85b1a9222c2d
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-10 10:58:21 -0500

    Update UtilsTest.php

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -40,9 +40,9 @@ final class UtilsTest extends TestCase
     {
         if (\version_compare(\PHP_VERSION, '7.2.0', '>='))
         {
-            $this->assertEquals((array) \hash_hmac_algos(), Utils::hashHmacAlgos());
+            $this->assertTrue(\hash_hmac_algos() === Utils::hashHmacAlgos());
         }
-        $this->assertEquals((array) \hash_algos(), Utils::hashHmacAlgos());
+        $this->assertTrue(\hash_algos() === Utils::hashHmacAlgos());
     }
   
 }

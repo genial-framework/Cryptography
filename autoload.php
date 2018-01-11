73d71483e8e3f9e3a5954421214fcfc3fd8d2bd1
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-11 01:42:58 -0500

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * @link      <https://github.com/Genial-Components/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
+ */
 
 //-----------------//
 // Prevents Errors //

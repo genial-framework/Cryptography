5c6ab9354196e694ba9a32f900564751aaafc999
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2018-01-11 01:31:53 -0500

    Update RuntimeException.php

--- src/Exception/RuntimeException.php
+++ src/Exception/RuntimeException.php
@@ -1,1 +1,16 @@
+<?php
+/**
+ * @link      <https://github.com/Genial-Components/Cryptography> for the canonical source repository.
+ * @copyright Copyright (c) 2017-2019 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Components/Cryptography/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Genial\Crypt\Exception;
 
+/**
+ * RuntimeException.
+ */
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{
+ 
+}
